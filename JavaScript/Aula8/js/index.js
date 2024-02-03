2d5f355a420d7d6888e5e04fbd52f7156080239b
fernandojsx <fjsx@hotmail.com>
2024-02-03 16:33:59 -0300

    Exercício da aula 8 com JavaScript

--- JavaScript/Aula8/js/index.js
+++ JavaScript/Aula8/js/index.js
@@ -2,7 +2,24 @@ const cepRegex = /[0-9]{8}/;
 
 cep.onkeyup = () => {
 
-    if(cepRegex.test(cep.value)){
-        alert('Deu certo');
+    if (cepRegex.test(cep.value)) {
+        try {
+            fetch(`https://viacep.com.br/ws/${cep.value}/json/`)
+                .then(response => response.json())
+                .then(response => {
+                    if (response.erro) {
+                        alert('O CEP está incorreto');
+                        cep.value = '';
+                        return;
+                    }
+                    logradouro.value = response.logradouro;
+                    bairro.value = response.bairro;
+                    localidade.value = response.localidade;
+                    uf.value = response.uf;
+                })
+
+        } catch (error) {
+            alert(error.message);
+        }
     }
 }
